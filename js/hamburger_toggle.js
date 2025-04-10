665681441e913efb28f452d1421092afad1d8f2b
arjcasadolago <arjcasadolago@gmail.com>
2025-04-10 13:43:12 +0100

    Add files via upload

--- js/hamburger_toggle.js
+++ js/hamburger_toggle.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
     const curtainMenu = document.getElementById('curtain-menu');
 
+    console.log("Dropdown script loaded");
     hamburger.addEventListener('click', function () {
+        console.log("Hamburger clicked!");
         this.classList.toggle('active');
         curtainMenu.classList.toggle('active');
     });
