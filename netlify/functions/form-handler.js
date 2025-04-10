bfdf33f73e6461acb7bbc38dfe2739492751573a
arjcasadolago <arjcasadolago@gmail.com>
2025-04-10 14:26:19 +0100

    Add files via upload

--- netlify/functions/form-handler.js
+++ netlify/functions/form-handler.js
@@ -23,14 +23,14 @@ exports.handler = async (event) => {
         to: email,
         subject: 'Obrigado pelo teu interesse nas aulas!',
         html: `
-      <p>Olá ${name},</p>
-      <p>Obrigado por mostrares interesse em começar aulas connosco! Estamos entusiasmados por saber de ti.</p>
-      <p>Neste momento, a melhor forma de nos contactares é através do nosso Instagram: <strong>@arj_casa_do_lago</strong>. Podes enviar-nos uma mensagem diretamente lá para conversarmos mais sobre as aulas.</p>
-      <p>Caso não tenhas Instagram, podes sempre contactar-nos através do número <strong>+351 913 901 120</strong>.</p>
-      <p>Aguardamos com expectativa para falar contigo em breve!</p>
-      <p>Melhores cumprimentos,<br/>
-      <strong>Alex Ryu Jitsu Casa do Lago</strong></p>
-    `,
+            <p>Olá ${name},</p>
+            <p>Obrigado por mostrares interesse em começar aulas connosco! Estamos entusiasmados por saber de ti.</p>
+            <p>Neste momento, a melhor forma de nos contactares é através do nosso Instagram: <strong>@arj_casa_do_lago</strong>. Podes enviar-nos uma mensagem diretamente lá para conversarmos mais sobre as aulas.</p>
+            <p>Caso não tenhas Instagram, podes sempre contactar-nos através do número <strong>+351 913 901 120</strong>.</p>
+            <p>Aguardamos com expectativa para falar contigo em breve!</p>
+            <p>Melhores cumprimentos,<br/>
+            <strong>Alex Ryu Jitsu Casa do Lago</strong></p>
+            `,
     });
 
     return {
